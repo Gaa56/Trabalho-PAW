d5d575d668f5576500d65a0c30fab910f887c916
Gaa56 <gabrielexmoreira@gmail.com>
2026-03-27 14:44:32 +0000

    MongoDb

--- paw_market/app.js
+++ paw_market/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +9,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Ligar ao MongoDb
+const connectDB = require('./config/mongodb');
+connectDB();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +28,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
